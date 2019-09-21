40ff8afe575f284a30dc1fe34220e56f010d4585
StevenVallarsa <orionsjv@gmail.com>
2019-09-21 18:24:38 -0400

    Adding get/set logic

--- School.cs
+++ School.cs
@@ -8,7 +8,46 @@ namespace SchoolLibrary
         public string City { get; set; }
         public string State { get; set; }
         public string Zip { get; set; }
-        public string Phone { get; set; }        
+        public string Phone { get; set; }
+
+        private string _twitter;
+        public string Twitter
+        {
+            get { return _twitter; }
+
+            set
+            {
+                if (value.StartsWith("@"))
+                {
+                    _twitter = value;
+                }
+                else
+                {
+                    throw new Exception("Twitter address must begin with @");
+                }
+            }
+         }
+
+        public School()
+        {
+            Name = "Untitled School";
+            Address = "0 No Street";
+            City = "No City";
+            State = "XX";
+            Zip = "00000";
+            Phone = "000-000-0000";
+        }
+
+        public School(string SchoolName, string SchoolPhone)
+        {
+            Name = SchoolName;
+            Address = "0 No Street";
+            City = "No City";
+            State = "XX";
+            Zip = "00000";
+            Phone = SchoolPhone;
+
+        }
 
     }
 }
